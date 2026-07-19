fd4dd7376294124890775bac3305b00e38ffc975
Alina Peng <alinapeng@Alinas-MacBook-Air.local>
2026-07-18 22:42:19 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./Header";
+import Article from "./Article";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+
+      <main>
+        <Article />
+      </main>
+
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Article.jsx
@@ -0,0 +1,47 @@
+function Article() {
+  return (
+    <>
+      <article>
+        <p>11/12/20</p>
+
+        <h2>On the Street in Brooklyn</h2>
+
+        <img src="/blog-image-1.jpg" alt="Woman wearing an orange dress" />
+
+        <p>
+          Cray ipsum, dolor sit amet consectetur adipisicing elit. Repellat
+          quidem deserunt nemo dignissimos fuga veniam inventore eligendi magnam
+          aperiam odit mollitia cupiditate eveniet velit officiis magni aliquid,
+          laborum dolor? Quasi? Veniam amet rerum ducimus est ea at neque alias.
+          Temporibus perspiciatis at impedit voluptas aut voluptates facere
+          totam aliquid debitis rerum, ex consequuntur, nulla eius quae corrupti
+          optio atque ut.
+        </p>
+
+        <a href="#">Continues...</a>
+      </article>
+
+      <article>
+        <p>11/11/20</p>
+
+        <h2>Vintage in Vogue</h2>
+
+        <img src="/blog-image-2.jpg" alt="People wearing vintage clothing"/>
+
+        <p>
+          Selfies sunt, dolor sit amet consectetur adipisicing elit. Repellat
+          quidem deserunt nemo dignissimos fuga veniam inventore eligendi magnam
+          aperiam odit mollitia cupiditate eveniet velit officiis magni aliquid,
+          laborum dolor? Quasi? Veniam amet rerum ducimus est ea at neque alias.
+          Temporibus perspiciatis at impedit voluptas aut voluptates facere
+          totam aliquid debitis rerum, ex consequuntur, nulla eius quae corrupti
+          optio atque ut.
+        </p>
+
+        <a href="#">Continues...</a>
+      </article>
+    </>
+  );
+}
+
+export default Article;
